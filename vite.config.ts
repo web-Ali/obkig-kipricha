d9696902ab4eb566c4e18bd360183fb3424f5f38
web-ali <suleymanov.web@gmail.com>
2024-07-03 11:45:44 +0300

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
+/* eslint-disable no-restricted-imports */
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/vite-deploy-demo/'
 })
