a0ae01e1ce150d0aa41184e32472044b9ac0cea4
web-ali <suleymanov.web@gmail.com>
2024-07-03 12:17:02 +0300

    fix

--- src/libs/route/routs.tsx
+++ src/libs/route/routs.tsx
@@ -1,13 +1,13 @@
 /* eslint-disable no-restricted-imports */
 import {
-    createBrowserRouter,
+  createHashRouter,
   } from "react-router-dom";
 
 import { NotFound404 } from "../../core/404";
 import { Layout } from "../../core/layout/layout";
 import { Main } from "../../core/main";
   
- export const router = createBrowserRouter([
+ export const router = createHashRouter([
   {
     element: <Layout />,
     children:[
